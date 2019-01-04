dd47e0777e2d86cdfa2bc484e4aed004d4bf3029
Hope_pdm <pengdemu@jiaeda.com>
2019-01-04 20:49:55 +0800

    搭好框架

---
+++ files.js
@@ -0,0 +1,14 @@
+var files = {
+    "threejs": [
+        "threejs_demo_air",
+        "threejs_demo_air1",
+    ],
+    "links": {
+        'showdoc':'https://www.showdoc.cc',
+        'baidu': 'https://www.baidu.com',
+        
+    } 
+        
+    
+	
+};
