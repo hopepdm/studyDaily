c478b1d20dad0d160bfb394185080207c9c3b23b
Hope_pdm <pengdemu@jiaeda.com>
2019-01-17 16:18:09 +0800

    add

--- files.js
+++ files.js
@@ -1,11 +1,12 @@
 var files = {
     "threejs": [
         "threejs_demo_game",
+		"threejs_demo_objexport"
     ],
     "links": {
         'showdoc': 'https://www.showdoc.cc',
         'ECMAScript': 'http://es6.ruanyifeng.com/',
-	'react小书': 'http://huziketang.mangojuice.top/books/react/lesson1',
+		'react小书': 'http://huziketang.mangojuice.top/books/react/lesson1',
     } 
         
     
