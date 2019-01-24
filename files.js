396a688c3dfcb8796fce983a7ea44292ac5f257e
Hope_pdm <pengdemu@jiaeda.com>
2019-01-24 10:30:26 +0800

    add

--- files.js
+++ files.js
@@ -7,6 +7,7 @@ var files = {
         'showdoc': 'https://www.showdoc.cc',
         'ECMAScript': 'http://es6.ruanyifeng.com/',
 		'react小书': 'http://huziketang.mangojuice.top/books/react/lesson1',
+		'sampleReact': 'https://github.com/hopepdm/dailyCoding/tree/master/simple-react',
     } 
         
     
